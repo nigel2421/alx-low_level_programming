f22d3b1041d22e7bf5f689dbfe4e7514b26bfb67
nigel2421 <nigel2421@yahoo.com>
2022-09-12 23:38:35 -0700

    loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ * 
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a' ; alpha <= 'z' : alpha++)
+	{
+		_putchar(alpha) ;
+	}
+	_putchar('\n');
+}
