807fd2eb62c1ac60796bf546890027e8b17736b2
Nigel <79846205+nigel2421@users.noreply.github.com>
2022-09-14 19:58:57 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,15 +6,27 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
+
 	int x = 1024, y, sum = 0;
+
 	for (y = 0; y < x; y++)
+
 	{
+
 		if ((y % 3 == 0) || (y % 5 == 0))
+
 		{
+
 			sum = sum + y;
+
 		}
+
 	}
+
 	printf("%d\n", sum);
+
 	return (0);
-}
+
+}	
