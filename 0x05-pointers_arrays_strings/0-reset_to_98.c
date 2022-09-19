aa62dc5468649d01866ba733abc3399848e23bb9
nigel2421 <nigel2421@yahoo.com>
2022-09-19 06:59:31 -0700

    week5

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Set pointer parameter to 98.
+ * and updates the value it points to to 98
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - calls other functions
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	0 - reset_to_98();
+	1 - swap();
+	2 - strlen();
+	3 - puts();
+	4 - print_rev();
+	5 - rev_string.c();
+	6 - puts2.c();
+	7 - puts();
+	8 - print_array();
+	9 - strcpy();
+	100 - atoi();
+	101 - keygen();
+}
