82e7ddc29f8371fe790c0bf7713fb63f245df3a0
nigel2421 <nigel2421@yahoo.com>
2022-09-26 01:33:23 -0700

    task3pointers

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include3 "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: string to change
+ * @to: string that should be copied
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
