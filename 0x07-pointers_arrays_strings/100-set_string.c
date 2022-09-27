75709574abdf60aed851b715e5caf3da1c963e88
Nigel <79846205+nigel2421@users.noreply.github.com>
2022-09-27 09:18:35 +0300

    hottest

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 #include "main.h"
-=======
-#include3 "main.h"
 #include <stdio.h>
->>>>>>> bf52924376d974747cee20f3e95f01d06ebcfcbd
 
 /**
  * set_string - sets the value of a pointer to a char
@@ -11,7 +7,7 @@
  * @to: The char.
  */
 
-void set_string(char **Bob Dylan, char *Robert Allen)
+void set_string(char **s, char *to)
 {
-	*Bob Dylan = Robert Allen;
+	*s = to;
 }
